13d9d74b5c3f28e5aa4e21f7a15016668bafcdd4
NabeFumi <takafumi.watanabe0522@gmail.com>
2021-12-07 20:09:03 -0800

    Initial Commit

---
+++ SwiftUIToDoApp/SwiftUIToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToDoAppApp.swift
+//  SwiftUIToDoApp
+//
+//  Created by Takafumi Watanabe on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
